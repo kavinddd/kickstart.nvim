430c5d231d7a33c448749873993507b80bc863e1
Win <fnvin@hotmail.com>
2025-07-07 22:46:01 +0700

    fix: bun

--- init.lua
+++ init.lua
@@ -160,6 +160,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- bun.js module not found when writing on hot reload
+vim.opt.backupcopy = 'yes'
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -926,7 +929,7 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
